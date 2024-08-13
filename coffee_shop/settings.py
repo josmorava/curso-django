2e1252d7c69fdc4660414177c3bd002b75daa841
Jose Morales <johndoe@example.com>
2024-08-12 20:08:49 -0400

    Crete new app /orders/

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     
     'products',
     'users',
+    'orders',
     
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
-# from django.contrib.auth import logout
-# from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
+from django.urls import reverse_lazy
 
-
-# def logout_view(request):
-#     logout(request)
-#     return redirect("logout")
+class RegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/register.html'
+    succes_url= reverse_lazy('login')  
